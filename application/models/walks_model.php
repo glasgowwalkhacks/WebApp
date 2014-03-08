168d01242364c5552b21438d5cf4911281ebee80
Kenny Coyle <kenny@heloo.net>
2014-03-08 15:21:15 +0000

    well that was silly

--- application/models/walks_model.php
+++ application/models/walks_model.php
@@ -21,6 +21,6 @@ class Walks_model extends MY_Model
 				WHERE (((acos(sin((".$latitude."*pi()/180)) * sin((`lat`*pi()/180))+cos((".$latitude."*pi()/180)) * cos((`lat`*pi()/180)) * cos(((".$longitude."- `lng`)*pi()/180))))*180/pi())*60*1.1515) <= ".$distance." 
                 order by (((acos(sin((".$latitude."*pi()/180)) * sin((`lat`*pi()/180))+cos((".$latitude."*pi()/180)) * cos((`lat`*pi()/180)) * cos(((".$longitude."- `lng`)*pi()/180))))*180/pi())*60*1.1515) asc";
 
-		return $ci->db->query($qry)->row();
+		return $ci->db->query($qry)->result_array();
     }
 }
