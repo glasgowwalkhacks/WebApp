a76a0c0fd70dcab56ba62d7d501734d807832ce7
Kenny Coyle <kenny@heloo.net>
2014-03-08 15:06:18 +0000

    simple search ?

--- index.php
+++ index.php
@@ -19,6 +19,8 @@
  *
  */
 	define('ENVIRONMENT', 'development');
+	error_reporting(E_ALL);
+	ini_set("display_errors", 1);
 /*
  *---------------------------------------------------------------
  * ERROR REPORTING
