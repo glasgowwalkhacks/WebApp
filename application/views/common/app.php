5f5a6e8fbf57f4998318a2b31f9b5040a1130199
Kenny Coyle <kenny@heloo.net>
2014-03-09 12:05:50 +0000

    small thing

--- application/views/common/app.php
+++ application/views/common/app.php
@@ -1,7 +1,29 @@
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <!--
+                            .,-:;//;:=,
+                        . :H@@@MM@M#H/.,+%;,
+                     ,/X+ +M@@M@MM%=,-%HMMM@X/,
+                   -+@MM; $M@@MH+-,;XMMMM@MMMM@+-
+                  ;@M@@M- XM@X;. -+XXXXXHHH@M@M#@/.
+                ,%MM@@MH ,@%=             .---=-=:=,.
+                =@#@@@MX.,                -%HX$$%%%:;
+               =-./@M@M$                   .;@MMMM@MM:
+               X@/ -$MM/     FOR SCIENCE    . +MM@@@M$
+              ,@M@H: :@:     -----------    . =X#@@@@-
+              ,@@@MMX, .     AND BREAKING   /H- ;@M@M=
+              .H@@@@M@+,     STUFF          %MM+..%#$.
+               /MMMM@MMH/.                  XM@MH; =;
+                /%+%$XHH@$=              , .H@@@@MX,
+                 .=--------.           -%H.,@@@@@MX,
+                 .%MM@@@HHHXX$$$%+- .:$MMX =M@@MM%.
+                   =XMMM@MM@MM#H;,-+HMM@M+ /MMMX=
+                     =%@M@M#@$-.=$@MM@@@M; %M%=
+                       ,:+$+-,/H#MMMMMMM@= =,
+                             =++%%%%+/:-.
+    -->
+
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
